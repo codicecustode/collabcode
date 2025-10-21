ce3bd102dafa3a52dcd064d6dae7ac6cbb169cee
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2025-10-21 13:37:48 +0530

    add App and main components to initialize the application

---
+++ web-client/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { EditorPage } from './pages/EditorPage';
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <EditorPage />
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
